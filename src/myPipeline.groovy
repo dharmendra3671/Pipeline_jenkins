db6ecc7020cc296f4d4ce4e0dd62583b584f171e
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-10 09:52:30 +0530

    Update myPipeline.groovy

--- src/myPipeline.groovy
+++ src/myPipeline.groovy
@@ -28,8 +28,8 @@ def call(Map pipelineParams) {
             }
         }
         post {
-            failure {
-                mail to: pipelineParams.email, subject: '', body: ""
+            always {
+                emailext to: pipelineParams.email, subject: 'Jenkins Pipeline Build Status', body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n ${currentBuild.currentResult}: Job ${env.JOB_NAME} \n ${currentBuild.currentResult}: ${env.BUILD_TAG}"
             }
         }
     }
